b5443edec4cdfa3549078538bc97fd56b375d7b6
R&D김원선 <bright1101@thesmc.co.kr>
2024-02-13 11:07:21 +0900

    route 예제 추가

--- app/page.tsx
+++ app/page.tsx
@@ -8,22 +8,42 @@ import {
   DrawerHeader,
   DrawerTitle,
   DrawerTrigger,
-} from "@/components/ui/drawer"
+} from "@/components/ui/drawer";
 
-export default function Home() {
-  return <main className="text-[100px]"><Drawer>
-  <DrawerTrigger>Open</DrawerTrigger>
-  <DrawerContent>
-    <DrawerHeader>
-      <DrawerTitle>Are you absolutely sure?</DrawerTitle>
-      <DrawerDescription>This action cannot be undone.</DrawerDescription>
-    </DrawerHeader>
-    <DrawerFooter>
-      <Button>Submit</Button>
-      <DrawerClose>
-        <Button variant="outline">Cancel</Button>
-      </DrawerClose>
-    </DrawerFooter>
-  </DrawerContent>
-</Drawer></main>;
+async function fetchData() {
+  try {
+    const response = await fetch("http://localhost:3000/api"); // API 엔드포인트 경로
+    if (response.ok) {
+      const data = await response.json();
+      return data.greeting;
+    } else {
+      console.error("Error fetching data");
+    }
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
+
+export default async function Home() {
+  const a = await fetchData();
+  return (
+    <main className="text-[100px]">
+      <div>{a}</div>
+      <Drawer>
+        <DrawerTrigger>hi</DrawerTrigger>
+        <DrawerContent>
+          <DrawerHeader>
+            <DrawerTitle>Are you absolutely sure?</DrawerTitle>
+            <DrawerDescription>This action cannot be undone.</DrawerDescription>
+          </DrawerHeader>
+          <DrawerFooter>
+            <Button>Submit</Button>
+            <DrawerClose>
+              <Button variant="outline">Cancel</Button>
+            </DrawerClose>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
+    </main>
+  );
 }
